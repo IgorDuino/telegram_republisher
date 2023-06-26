fce0a6da8137bcfd0812a1ec7e037292dc8e2dc9
Igor Kuzmenkov <me@igorduino.ru>
2023-06-26 19:44:02 +0300

    fix filling order#13

--- userbot/utils/utils.py
+++ userbot/utils/utils.py
@@ -22,12 +22,32 @@ async def get_admined_and_possible_donor_channels(client):
 
 
 async def fill_channel(client: tg.Client, donor_id, recipient_id, limit):
+    history = []
     async for message in client.get_chat_history(donor_id, limit=limit):
+        history.append(message)
+    history = history[::-1]
+    for message in history:
         message: tg.types.Message
         try:
-            await bypass_copy(message, recipient_id)
+            try:
+                if message.media_group_id:
+                    await message._client.copy_media_group(
+                        chat_id=recipient_id,
+                        from_chat_id=donor_id,
+                        message_ids=message.message_id,
+                    )
+                else:
+                    await message.copy(chat_id=recipient_id)
+
+            except tg.errors.exceptions.bad_request_400.ChatForwardsRestricted:
+                logger.warning(f"Chat {recipient_id} does not allow forwards, trying to bypass...")
+                await bypass_copy(
+                    message,
+                    recipient_id,
+                )
+
         except Exception as e:
             logger.error(f"Error while copying message: {e}")
             continue
-        
+
         await asyncio.sleep(0.1)
